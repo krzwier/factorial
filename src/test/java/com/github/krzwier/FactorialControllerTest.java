94defa178a39186f75efee8b3731ef0b3848f814
krzwier <krzwier@gmail.com>
2020-04-18 13:53:00 -0500

    Finish simple webservice with 200 and 400 response status.

--- src/test/java/com/github/krzwier/FactorialControllerTest.java
+++ src/test/java/com/github/krzwier/FactorialControllerTest.java
@@ -37,5 +37,28 @@ public class FactorialControllerTest {
         assertEquals(200,status);
     }
 
+    @Test
+    void FactorialController_Input100_ExpectHuge() throws Exception {
+        MvcResult result = mockmvc.perform(get("/api/factorial?input=100")).andReturn();
+        String content = result.getResponse().getContentAsString();
+        assertEquals("9332621544394415268169923885626670049071596826438162146859"
+            + "29638952175999932299156089414639761565182862536979208272237582511852109168640"
+            + "00000000000000000000000",content);
+    }
+
+    @Test
+    void FactorialController_InputEmpty_Returns400() throws Exception {
+        MvcResult result = mockmvc.perform(get("/api/factorial?input=")).andReturn();
+        int status = result.getResponse().getStatus();
+        assertEquals(400,status);
+    }
+
+    @Test
+    void FactorialController_InputHello_Returns400() throws Exception {
+        MvcResult result = mockmvc.perform(get("/api/factorial?input=hello")).andReturn();
+        int status = result.getResponse().getStatus();
+        assertEquals(400,status);
+    }
+
 
 }
